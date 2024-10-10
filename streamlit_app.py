4471560dee4404f48a3766811f1da0d445424205
JohnVan77 <john.p.vanderploeg@gmail.com>
2024-10-10 13:09:50 +1030

    Update streamlit_app.py
    Reformat of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+#import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customise Your Smoothie! :balloon:")
@@ -50,6 +51,11 @@ if ingredients_list:  # Cleaning Up Empty Brackets
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        # Display fruityvice nutrition information
+        #note: issue with fruityvice.com - disabling for now!
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        ##st.text(fruityvice_response.json())
+        #fv_df = st.datafame(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -67,10 +73,3 @@ if ingredients_list:  # Cleaning Up Empty Brackets
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-    # Display fruityvice nutrition information
-    #note: issue with fruityvice.com - disabling for now!
-    #import requests
-    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    ##st.text(fruityvice_response.json())
-    #fv_df = st.datafame(data=fruityvice_response.json(), use_container_width=True)
